90c4799ee37467df86941ccdb21ea6e3aaddc6bd
cebed <cebed@student.chalmers.se>
2023-03-27 14:52:05 +0200

    Revert "Access control"
    This reverts commit 9f7d57bca75918def386397ce3cc7db48108fcfb.

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -93,8 +93,6 @@ const ContactUs = ({ contactUsRef }: ContactUsProps) => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Headers': 'Content-Type',
         },
         body: JSON.stringify({
           token: captchaToken,
