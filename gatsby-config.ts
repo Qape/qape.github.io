8ab066cd7c13c642606176b2a266d42d932b3e95
cebed <daniel_97_c@hotmail.com>
2023-03-29 22:00:23 +0200

    Update config path prefix

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -12,7 +12,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  pathPrefix: '/qape.github.io',
+  pathPrefix: '/',
   plugins: [
     {
       resolve: `gatsby-plugin-env-variables`,
